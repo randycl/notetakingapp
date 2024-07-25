4a1deed1167cb18440189819eb2b3a4fd63a95f4
randycl <125942278+randycl@users.noreply.github.com>
2024-07-25 11:43:01 -0500

    Updated TestController.java

--- note-taking-app/note-taking-app/src/test/java/com/randy_tech/notetakingapp/TestController.java
+++ note-taking-app/note-taking-app/src/test/java/com/randy_tech/notetakingapp/TestController.java
@@ -1,5 +1,10 @@
 package com.randy_tech.notetakingapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class TestController {
 
